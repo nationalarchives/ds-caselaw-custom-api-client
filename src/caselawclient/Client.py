494c1f7e4bfd81705ced8b9cac34b6a3d128afb7
Laura Porter <laura@dxw.com>
2022-04-26 16:13:25 +0100

    Set Judgment metadata date using XQuery
    Set the Judgment metadata date directly in the Judgment XML document, without
    deserialising and reserialising the Judgment XML
    
    TODO: Refactor these to be more generic. The issue here is that passing in an
    XPath to the node to be updated as a string does not work, as XQuery expects
    the XPath to be a node()*, not a string, and I'm not sure how to cast a
    string to a node()*, or how to pass the Xpath in as a node()*

--- src/caselawclient/Client.py
+++ src/caselawclient/Client.py
@@ -184,6 +184,17 @@ class MarklogicApiClient:
         )
         return response
 
+    def set_judgment_date(self, judgment_uri, content):
+        uri = f"/{judgment_uri.lstrip('/')}.xml"
+        xquery_path = os.path.join(
+            ROOT_DIR, "xquery", "set_metadata_date.xqy"
+        )
+
+        response = self.eval(
+            xquery_path, vars=f'{{"uri":"{uri}", "content":"{content}"}}', accept_header="application/xml"
+        )
+        return response
+
     def save_judgment_xml(self, judgment_uri: str, judgment_xml: Element) -> requests.Response:
         xml = ElementTree.tostring(judgment_xml)
 
