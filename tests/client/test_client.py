abd167a612f0b15fd51360dcf0f63512ab5748d2
Laura Porter <laura@dxw.com>
2022-08-25 12:03:52 +0100

    Add a test for invoke() to the test_client
    We weren't testing this method, which has slightly different parameters to
    eval()

--- tests/client/test_client.py
+++ tests/client/test_client.py
@@ -24,3 +24,23 @@ class ApiClientTest(unittest.TestCase):
             },
             data={"xquery": "mock-query", "vars": '{{"testvar":"test"}}'},
         )
+
+    @patch("src.caselawclient.Client.Path")
+    def test_invoke_calls_request(self, MockPath):
+        mock_path_instance = MockPath.return_value
+        mock_path_instance.read_text.return_value = "mock-query"
+
+        client = MarklogicApiClient("", "", "", False)
+        client.session.request = MagicMock()
+
+        client.invoke("mock-query-path.xqy", vars='{{"testvar":"test"}}')
+
+        client.session.request.assert_called_with(
+            "POST",
+            url=client._path_to_request_url("LATEST/invoke"),
+            headers={
+                "Content-type": "application/x-www-form-urlencoded",
+                "Accept": "multipart/mixed",
+            },
+            data={"module": "mock-query-path.xqy", "vars": '{{"testvar":"test"}}'},
+        )
