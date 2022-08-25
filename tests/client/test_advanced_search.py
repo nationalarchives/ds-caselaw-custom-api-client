57a44c7d6b01c4efc3eaea1068e7aeceb873e689
Laura Porter <laura@dxw.com>
2022-08-25 15:20:07 +0100

    Reduce repeated assertions in advanced_search tests
    Only one of these tests needs to assert the full set of vars passed to
    client.advanced_search(). The other two tests need only check values which vary
    according to test conditions.

--- tests/client/test_advanced_search.py
+++ tests/client/test_advanced_search.py
@@ -51,6 +51,7 @@ class TestAdvancedSearch(unittest.TestCase):
     def test_advanced_search_user_can_view_unpublished_and_show_unpublished_is_true(
         self,
     ):
+        """The user is permitted to see unpublished judgments and requests to see unpublished judgments"""
         client = MarklogicApiClient("", "", "", False)
 
         with patch.object(client, "invoke"):
@@ -66,26 +67,7 @@ class TestAdvancedSearch(unittest.TestCase):
                     page_size=20,
                     show_unpublished=True,
                 )
-
-                expected_vars = {
-                    "court": "ewhc",
-                    "judge": "a. judge",
-                    "page": 2,
-                    "page-size": 20,
-                    "q": "my-query",
-                    "party": "a party",
-                    "neutral_citation": "",
-                    "specific_keyword": "",
-                    "order": "",
-                    "from": "",
-                    "to": "",
-                    "show_unpublished": "true",
-                    "only_unpublished": "false",
-                }
-
-                client.invoke.assert_called_with(
-                    "/judgments/search/search.xqy", json.dumps(expected_vars)
-                )
+                assert '"show_unpublished": "true"' in client.invoke.call_args.args[1]
 
     def test_advanced_search_user_cannot_view_unpublished_but_show_unpublished_is_true(
         self,
@@ -109,23 +91,7 @@ class TestAdvancedSearch(unittest.TestCase):
                         show_unpublished=True,
                     )
 
-                    expected_vars = {
-                        "court": "ewhc",
-                        "judge": "a. judge",
-                        "page": 2,
-                        "page-size": 20,
-                        "q": "my-query",
-                        "party": "a party",
-                        "neutral_citation": "",
-                        "specific_keyword": "",
-                        "order": "",
-                        "from": "",
-                        "to": "",
-                        "show_unpublished": "false",
-                        "only_unpublished": "false",
-                    }
-
-                    client.invoke.assert_called_with(
-                        "/judgments/search/search.xqy", json.dumps(expected_vars)
+                    assert (
+                        '"show_unpublished": "false"' in client.invoke.call_args.args[1]
                     )
                     mock_logging.assert_called()
