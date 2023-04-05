9ae6762f6cbb25f0dcda399e217b89eab9a72767
Nick Jackson <nick@dxw.com>
2023-04-05 12:04:33 +0100

    Move Judgment object from EUI
    This is a fairly blind copying of the Judgment model and its associated
    tests, so we can begin the process of decoupling EUI from its existing
    implementation and toward using this. Some methods haven't been copied
    because they are (at least for the moment) strongly coupled to the way
    EUI understands judgments.
    
    The lack of tests for AWS is a known problem, but outside the scope of
    this PR.

---
+++ src/caselawclient/models/judgments.py
@@ -0,0 +1,182 @@
+import datetime
+from functools import cached_property
+
+from requests_toolbelt.multipart import decoder
+
+from src.caselawclient.Client import MarklogicApiClient
+
+from .utilities import get_judgment_root, render_versions
+from .utilities.aws import (
+    generate_docx_url,
+    generate_pdf_url,
+    notify_changed,
+    publish_documents,
+    unpublish_documents,
+    uri_for_s3,
+)
+
+JUDGMENT_STATUS_HOLD = "On hold"
+JUDGMENT_STATUS_PUBLISHED = "Published"
+JUDGMENT_STATUS_IN_PROGRESS = "In progress"
+
+
+class CannotPublishUnpublishableJudgment(Exception):
+    pass
+
+
+class Judgment:
+    def __init__(self, uri: str, api_client: MarklogicApiClient):
+        self.uri = uri.strip("/")
+        self.api_client = api_client
+
+        # As part of initialisation, we preload the NCN so we can generate a MarklogicResourceNotFoundError early
+        self.neutral_citation
+
+    @property
+    def public_uri(self) -> str:
+        return "https://caselaw.nationalarchives.gov.uk/{uri}".format(uri=self.uri)
+
+    @cached_property
+    def neutral_citation(self) -> str:
+        return self.api_client.get_judgment_citation(self.uri)
+
+    @cached_property
+    def name(self) -> str:
+        return self.api_client.get_judgment_name(self.uri)
+
+    @cached_property
+    def court(self) -> str:
+        return self.api_client.get_judgment_court(self.uri)
+
+    @cached_property
+    def judgment_date_as_string(self) -> str:
+        return self.api_client.get_judgment_work_date(self.uri)
+
+    @cached_property
+    def judgment_date_as_date(self) -> datetime.date:
+        return datetime.datetime.strptime(
+            self.judgment_date_as_string, "%Y-%m-%d"
+        ).date()
+
+    @cached_property
+    def is_published(self) -> bool:
+        return self.api_client.get_published(self.uri)
+
+    @cached_property
+    def is_held(self) -> bool:
+        return self.api_client.get_property(self.uri, "editor-hold") == "true"
+
+    @cached_property
+    def is_sensitive(self) -> bool:
+        return self.api_client.get_sensitive(self.uri)
+
+    @cached_property
+    def is_anonymised(self) -> bool:
+        return self.api_client.get_anonymised(self.uri)
+
+    @cached_property
+    def has_supplementary_materials(self) -> bool:
+        return self.api_client.get_supplemental(self.uri)
+
+    @cached_property
+    def source_name(self) -> str:
+        return self.api_client.get_property(self.uri, "source-name")
+
+    @cached_property
+    def source_email(self) -> str:
+        return self.api_client.get_property(self.uri, "source-email")
+
+    @cached_property
+    def consignment_reference(self) -> str:
+        return self.api_client.get_property(self.uri, "transfer-consignment-reference")
+
+    @property
+    def docx_url(self) -> str:
+        return generate_docx_url(uri_for_s3(self.uri))
+
+    @property
+    def pdf_url(self) -> str:
+        return generate_pdf_url(uri_for_s3(self.uri))
+
+    @cached_property
+    def assigned_to(self) -> str:
+        return self.api_client.get_property(self.uri, "assigned-to")
+
+    @cached_property
+    def versions(self) -> list:
+        versions_response = self.api_client.list_judgment_versions(self.uri)
+
+        try:
+            decoded_versions = decoder.MultipartDecoder.from_response(versions_response)
+            return render_versions(decoded_versions.parts)
+        except AttributeError:
+            return []
+
+    def content_as_xml(self) -> str:
+        return self.api_client.get_judgment_xml(self.uri, show_unpublished=True)
+
+    def content_as_html(self, version_uri: str) -> str:
+        results = self.api_client.eval_xslt(
+            self.uri, version_uri, show_unpublished=True
+        )
+        multipart_data = decoder.MultipartDecoder.from_response(results)
+        return multipart_data.parts[0].text
+
+    @cached_property
+    def is_failure(self) -> bool:
+        if "failures" in self.uri:
+            return True
+        return False
+
+    @cached_property
+    def is_editable(self) -> bool:
+        if "error" in self._get_root():
+            return False
+        return True
+
+    def _get_root(self) -> str:
+        return get_judgment_root(self.content_as_xml())
+
+    @cached_property
+    def is_publishable(self) -> bool:
+        if self.is_held:
+            return False
+
+        return True
+
+    @property
+    def status(self) -> str:
+        if self.is_published:
+            return JUDGMENT_STATUS_PUBLISHED
+
+        if self.is_held:
+            return JUDGMENT_STATUS_HOLD
+
+        return JUDGMENT_STATUS_IN_PROGRESS
+
+    def publish(self) -> None:
+        if not self.is_publishable:
+            raise CannotPublishUnpublishableJudgment
+
+        publish_documents(uri_for_s3(self.uri))
+        self.api_client.set_published(self.uri, True)
+        notify_changed(
+            uri=self.uri,
+            status="published",
+            enrich=True,
+        )
+
+    def unpublish(self) -> None:
+        unpublish_documents(uri_for_s3(self.uri))
+        self.api_client.set_published(self.uri, False)
+        notify_changed(
+            uri=self.uri,
+            status="not published",
+            enrich=False,
+        )
+
+    def hold(self) -> None:
+        self.api_client.set_property(self.uri, "editor-hold", "true")
+
+    def unhold(self) -> None:
+        self.api_client.set_property(self.uri, "editor-hold", "false")
---
+++ tests/models/test_judgments.py
@@ -0,0 +1,258 @@
+import datetime
+from unittest.mock import Mock, patch
+
+import pytest
+
+from src.caselawclient.Client import MarklogicApiClient
+from src.caselawclient.models.judgments import (
+    JUDGMENT_STATUS_HOLD,
+    JUDGMENT_STATUS_IN_PROGRESS,
+    JUDGMENT_STATUS_PUBLISHED,
+    CannotPublishUnpublishableJudgment,
+    Judgment,
+)
+
+
+@pytest.fixture
+def mock_api_client():
+    return Mock(spec=MarklogicApiClient)
+
+
+class TestJudgment:
+    def test_uri_strips_slashes(self, mock_api_client):
+        judgment = Judgment("////test/1234/////", mock_api_client)
+
+        assert judgment.uri == "test/1234"
+
+    def test_public_uri(self, mock_api_client):
+        judgment = Judgment("test/1234", mock_api_client)
+
+        assert (
+            judgment.public_uri == "https://caselaw.nationalarchives.gov.uk/test/1234"
+        )
+
+    def test_judgment_neutral_citation(self, mock_api_client):
+        mock_api_client.get_judgment_citation.return_value = "[2023] TEST 1234"
+
+        judgment = Judgment("test/1234", mock_api_client)
+
+        assert judgment.neutral_citation == "[2023] TEST 1234"
+        mock_api_client.get_judgment_citation.assert_called_once_with("test/1234")
+
+    def test_judgment_name(self, mock_api_client):
+        mock_api_client.get_judgment_name.return_value = (
+            "Test Judgment v Test Judgement"
+        )
+
+        judgment = Judgment("test/1234", mock_api_client)
+
+        assert judgment.name == "Test Judgment v Test Judgement"
+        mock_api_client.get_judgment_name.assert_called_once_with("test/1234")
+
+    def test_judgment_court(self, mock_api_client):
+        mock_api_client.get_judgment_court.return_value = "Court of Testing"
+
+        judgment = Judgment("test/1234", mock_api_client)
+
+        assert judgment.court == "Court of Testing"
+        mock_api_client.get_judgment_court.assert_called_once_with("test/1234")
+
+    def test_judgment_date_as_string(self, mock_api_client):
+        mock_api_client.get_judgment_work_date.return_value = "2023-02-03"
+
+        judgment = Judgment("test/1234", mock_api_client)
+
+        assert judgment.judgment_date_as_string == "2023-02-03"
+        assert judgment.judgment_date_as_date == datetime.date(2023, 2, 3)
+        mock_api_client.get_judgment_work_date.assert_called_once_with("test/1234")
+
+    def test_judgment_is_published(self, mock_api_client):
+        mock_api_client.get_published.return_value = True
+
+        judgment = Judgment("test/1234", mock_api_client)
+
+        assert judgment.is_published is True
+        mock_api_client.get_published.assert_called_once_with("test/1234")
+
+    def test_judgment_is_held(self, mock_api_client):
+        mock_api_client.get_property.return_value = False
+
+        judgment = Judgment("test/1234", mock_api_client)
+
+        assert judgment.is_held is False
+        mock_api_client.get_property.assert_called_once_with("test/1234", "editor-hold")
+
+    def test_judgment_is_sensitive(self, mock_api_client):
+        mock_api_client.get_sensitive.return_value = True
+
+        judgment = Judgment("test/1234", mock_api_client)
+
+        assert judgment.is_sensitive is True
+        mock_api_client.get_sensitive.assert_called_once_with("test/1234")
+
+    def test_judgment_is_anonymised(self, mock_api_client):
+        mock_api_client.get_anonymised.return_value = True
+
+        judgment = Judgment("test/1234", mock_api_client)
+
+        assert judgment.is_anonymised is True
+        mock_api_client.get_anonymised.assert_called_once_with("test/1234")
+
+    def test_judgment_has_supplementary_materials(self, mock_api_client):
+        mock_api_client.get_supplemental.return_value = True
+
+        judgment = Judgment("test/1234", mock_api_client)
+
+        assert judgment.has_supplementary_materials is True
+        mock_api_client.get_supplemental.assert_called_once_with("test/1234")
+
+    def test_judgment_source_name(self, mock_api_client):
+        mock_api_client.get_property.return_value = "Test Name"
+
+        judgment = Judgment("test/1234", mock_api_client)
+
+        assert judgment.source_name == "Test Name"
+        mock_api_client.get_property.assert_called_once_with("test/1234", "source-name")
+
+    def test_judgment_source_email(self, mock_api_client):
+        mock_api_client.get_property.return_value = "testemail@example.com"
+
+        judgment = Judgment("test/1234", mock_api_client)
+
+        assert judgment.source_email == "testemail@example.com"
+        mock_api_client.get_property.assert_called_once_with(
+            "test/1234", "source-email"
+        )
+
+    def test_judgment_consignment_reference(self, mock_api_client):
+        mock_api_client.get_property.return_value = "TDR-2023-ABC"
+
+        judgment = Judgment("test/1234", mock_api_client)
+
+        assert judgment.consignment_reference == "TDR-2023-ABC"
+        mock_api_client.get_property.assert_called_once_with(
+            "test/1234", "transfer-consignment-reference"
+        )
+
+    @patch("src.caselawclient.models.judgments.generate_docx_url")
+    def test_judgment_docx_url(self, mock_url_generator, mock_api_client):
+        mock_url_generator.return_value = "https://example.com/mock.docx"
+
+        judgment = Judgment("test/1234", mock_api_client)
+
+        assert judgment.docx_url == "https://example.com/mock.docx"
+        mock_url_generator.assert_called_once
+
+    @patch("src.caselawclient.models.judgments.generate_pdf_url")
+    def test_judgment_pdf_url(self, mock_url_generator, mock_api_client):
+        mock_url_generator.return_value = "https://example.com/mock.pdf"
+
+        judgment = Judgment("test/1234", mock_api_client)
+
+        assert judgment.pdf_url == "https://example.com/mock.pdf"
+        mock_url_generator.assert_called_once
+
+    def test_judgment_assigned_to(self, mock_api_client):
+        mock_api_client.get_property.return_value = "testuser"
+
+        judgment = Judgment("test/1234", mock_api_client)
+
+        assert judgment.assigned_to == "testuser"
+        mock_api_client.get_property.assert_called_once_with("test/1234", "assigned-to")
+
+    def test_judgment_content_as_xml(self, mock_api_client):
+        mock_api_client.get_judgment_xml.return_value = "<xml></xml>"
+
+        judgment = Judgment("test/1234", mock_api_client)
+
+        assert judgment.content_as_xml() == "<xml></xml>"
+        mock_api_client.get_judgment_xml.assert_called_once_with(
+            "test/1234", show_unpublished=True
+        )
+
+    def test_judgment_is_failure(self, mock_api_client):
+        successful_judgment = Judgment("test/1234", mock_api_client)
+        failing_judgment = Judgment("failures/test/1234", mock_api_client)
+
+        assert successful_judgment.is_failure is False
+        assert failing_judgment.is_failure is True
+
+    def test_judgment_status(self, mock_api_client):
+        in_progress_judgment = Judgment("test/1234", mock_api_client)
+        in_progress_judgment.is_held = False
+        in_progress_judgment.is_published = False
+        assert in_progress_judgment.status == JUDGMENT_STATUS_IN_PROGRESS
+
+        on_hold_judgment = Judgment("test/1234", mock_api_client)
+        on_hold_judgment.is_held = True
+        on_hold_judgment.is_published = False
+        assert on_hold_judgment.status == JUDGMENT_STATUS_HOLD
+
+        published_judgment = Judgment("test/1234", mock_api_client)
+        on_hold_judgment.is_held = False
+        published_judgment.is_published = True
+        assert published_judgment.status == JUDGMENT_STATUS_PUBLISHED
+
+
+class TestJudgmentPublication:
+    def test_judgment_is_publishable_if_held(self, mock_api_client):
+        judgment = Judgment("test/1234", mock_api_client)
+        judgment.is_held = True
+
+        assert judgment.is_publishable is False
+
+    def test_judgment_is_publishable_if_not_held(self, mock_api_client):
+        judgment = Judgment("test/1234", mock_api_client)
+        judgment.is_held = False
+
+        assert judgment.is_publishable is True
+
+    def test_publish_fails_if_not_publishable(self, mock_api_client):
+        with pytest.raises(CannotPublishUnpublishableJudgment):
+            judgment = Judgment("test/1234", mock_api_client)
+            judgment.is_publishable = False
+            judgment.publish()
+            mock_api_client.set_published.assert_not_called()
+
+    @patch("src.caselawclient.models.judgments.notify_changed")
+    @patch("src.caselawclient.models.judgments.publish_documents")
+    def test_publish(
+        self, mock_publish_documents, mock_notify_changed, mock_api_client
+    ):
+        judgment = Judgment("test/1234", mock_api_client)
+        judgment.is_publishable = True
+        judgment.publish()
+        mock_publish_documents.assert_called_once_with("test/1234")
+        mock_api_client.set_published.assert_called_once_with("test/1234", True)
+        mock_notify_changed.assert_called_once_with(
+            uri="test/1234", status="published", enrich=True
+        )
+
+    @patch("src.caselawclient.models.judgments.notify_changed")
+    @patch("src.caselawclient.models.judgments.unpublish_documents")
+    def test_unpublish(
+        self, mock_unpublish_documents, mock_notify_changed, mock_api_client
+    ):
+        judgment = Judgment("test/1234", mock_api_client)
+        judgment.unpublish()
+        mock_unpublish_documents.assert_called_once_with("test/1234")
+        mock_api_client.set_published.assert_called_once_with("test/1234", False)
+        mock_notify_changed.assert_called_once_with(
+            uri="test/1234", status="not published", enrich=False
+        )
+
+
+class TestJudgmentHold:
+    def test_hold(self, mock_api_client):
+        judgment = Judgment("test/1234", mock_api_client)
+        judgment.hold()
+        mock_api_client.set_property.assert_called_once_with(
+            "test/1234", "editor-hold", "true"
+        )
+
+    def test_unhold(self, mock_api_client):
+        judgment = Judgment("test/1234", mock_api_client)
+        judgment.unhold()
+        mock_api_client.set_property.assert_called_once_with(
+            "test/1234", "editor-hold", "false"
+        )
