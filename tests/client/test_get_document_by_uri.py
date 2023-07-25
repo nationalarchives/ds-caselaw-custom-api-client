4e040af773c8726ab94735f7668c5d143976aee0
Nick Jackson <nick@dxw.com>
2023-07-25 12:08:21 +0100

    get_document_by_uri now returns correct type of document
    This uses the new `test_get_document_type_from_uri` method which checks
    the collections a document in part of, and returns the correct class for
    `get_document_by_uri` to instantiate an instance of.

---
+++ tests/client/test_get_document_by_uri.py
@@ -0,0 +1,65 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+import pytest
+
+from caselawclient.Client import (
+    DOCUMENT_COLLECTION_URI_JUDGMENT,
+    DOCUMENT_COLLECTION_URI_PRESS_SUMMARY,
+    DocumentHasNoTypeCollection,
+    MarklogicApiClient,
+)
+from caselawclient.models.judgments import Judgment
+from caselawclient.models.press_summaries import PressSummary
+
+
+class TestGetDocumentByUri(TestCase):
+    def setUp(self):
+        self.client = MarklogicApiClient("", "", "", False)
+
+    @patch("caselawclient.Client.Judgment", autospec=True)
+    @patch("caselawclient.Client.MarklogicApiClient.get_document_type_from_uri")
+    def test_get_document_by_uri(self, mock_get_document_type, mock_judgment):
+        mock_get_document_type.return_value = mock_judgment
+
+        document = self.client.get_document_by_uri(uri="test/1234")
+
+        mock_get_document_type.assert_called_with("test/1234")
+        mock_judgment.assert_called_with("test/1234", self.client)
+
+        self.assertIsInstance(document, Judgment)
+
+
+class TestGetDocumentTypeFromUri(TestCase):
+    def setUp(self):
+        self.client = MarklogicApiClient("", "", "", False)
+
+    @patch("caselawclient.Client.MarklogicApiClient._send_to_eval")
+    @patch("caselawclient.Client.get_multipart_strings_from_marklogic_response")
+    def test_get_document_type_from_uri_if_judgment(
+        self, mock_get_marklogic_response, mock_eval
+    ):
+        mock_get_marklogic_response.return_value = [DOCUMENT_COLLECTION_URI_JUDGMENT]
+        document_type = self.client.get_document_type_from_uri(uri="test/1234")
+        assert document_type == Judgment
+
+    @patch("caselawclient.Client.MarklogicApiClient._send_to_eval")
+    @patch("caselawclient.Client.get_multipart_strings_from_marklogic_response")
+    def test_get_document_type_from_uri_if_press_summary(
+        self, mock_get_marklogic_response, mock_eval
+    ):
+        mock_get_marklogic_response.return_value = [
+            DOCUMENT_COLLECTION_URI_PRESS_SUMMARY
+        ]
+        document_type = self.client.get_document_type_from_uri(uri="test/1234")
+        assert document_type == PressSummary
+
+    @patch("caselawclient.Client.MarklogicApiClient._send_to_eval")
+    @patch("caselawclient.Client.get_multipart_strings_from_marklogic_response")
+    def test_get_document_type_from_uri_if_no_valid_collection(
+        self, mock_get_marklogic_response, mock_eval
+    ):
+        mock_get_marklogic_response.return_value = []
+
+        with pytest.raises(DocumentHasNoTypeCollection):
+            self.client.get_document_type_from_uri(uri="test/1234")
