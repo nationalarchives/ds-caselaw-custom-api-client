08fec9e361cd4ca4e70c146cc31e74da4d6ec907
David McKee <david.mckee@dxw.com>
2023-09-18 15:36:25 +0100

    Add 'new' status to Document where it is unassigned

--- src/caselawclient/models/documents.py
+++ src/caselawclient/models/documents.py
@@ -39,7 +39,12 @@ DOCUMENT_STATUS_PUBLISHED = "Published"
 """ This document has been published and should be considered publicly visible. """
 
 DOCUMENT_STATUS_IN_PROGRESS = "In progress"
-""" This document has not been published or put on hold, and should be progressing through the document pipeline. """
+""" This document has not been published or put on hold, and has been picked up by an editor and
+    should be progressing through the document pipeline. """
+
+DOCUMENT_STATUS_NEW = "New"
+""" This document isn't published, on hold, or assigned, and can be picked up by an editor in the future. """
+
 
 DOCUMENT_COLLECTION_URI_JUDGMENT = "judgment"
 DOCUMENT_COLLECTION_URI_PRESS_SUMMARY = "press-summary"
@@ -386,7 +391,10 @@ class Document:
         if self.is_held:
             return DOCUMENT_STATUS_HOLD
 
-        return DOCUMENT_STATUS_IN_PROGRESS
+        if self.assigned_to:
+            return DOCUMENT_STATUS_IN_PROGRESS
+
+        return DOCUMENT_STATUS_NEW
 
     def enrich(self) -> None:
         notify_changed(
