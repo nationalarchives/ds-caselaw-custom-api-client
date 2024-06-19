963169d3fc8546108bf9b0ca8e0cd26e0fb557b2
Dragon Dave McKee <david.mckee@dxw.com>
2024-06-19 10:18:03 +0100

    Make enrichment date maths timezone aware

---
+++ tests/models/utilities/test_util_dates.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from pytz import timezone
+
+from caselawclient.models.utilities.dates import parse_string_date_as_utc
+
+TOKYO = timezone("Asia/Tokyo")
+PLUS_4 = timezone("Etc/GMT+4")
+LONDON = timezone("Europe/London")
+
+
+def test_parse_string_date():
+    assert parse_string_date_as_utc("2002-06-01T12:00:00Z", LONDON) == datetime.fromisoformat(
+        "2002-06-01T12:00:00+00:00"
+    )
+    assert parse_string_date_as_utc("2002-01-01T12Z", LONDON) == datetime.fromisoformat("2002-01-01T12:00:00+00:00")
+    assert parse_string_date_as_utc("2002-06-01T13", LONDON) == datetime.fromisoformat("2002-06-01T12:00:00+00:00")
+    assert parse_string_date_as_utc("2002-01-01T13", LONDON) == datetime.fromisoformat("2002-01-01T13:00:00+00:00")
+    assert parse_string_date_as_utc("2002-06-01T12-05:00", LONDON) == datetime.fromisoformat(
+        "2002-06-01T17:00:00+00:00"
+    )
+    assert parse_string_date_as_utc("2002-06-01T12-05:00", TOKYO) == datetime.fromisoformat("2002-06-01T17:00:00+00:00")
+    assert parse_string_date_as_utc("2002", timezone=TOKYO) == datetime.fromisoformat("2001-12-31T15:00:00+00:00")
+    assert parse_string_date_as_utc("2002", timezone=PLUS_4) == datetime.fromisoformat("2002-01-01T04:00:00+00:00")
