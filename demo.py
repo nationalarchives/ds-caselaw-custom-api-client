531564b7930683c7addb86aee57e71b6ed7ae1f1
Dragon Dave McKee <david.mckee@dxw.com>
2024-06-26 10:54:11 +0100

    wip

--- demo.py
+++ demo.py
@@ -17,18 +17,17 @@ api_client = Client.MarklogicApiClient(
     use_https=env("MARKLOGIC_USE_HTTPS", default=None),
 )
 
-print(Client.__file__)
-print(api_client.get_linked_caselaw("uksc/2024/1"))
+# print(Client.__file__)
+# print(api_client.get_linked_caselaw("uksc/2024/1"))
 
-exit()
 raw_bytes = Client.get_single_bytestring_from_marklogic_response(
     api_client.accessible_judgment_transformation("uksc/2024/1", show_unpublished=True)
 )
 root = lxml.html.fromstring(raw_bytes)
 print(raw_bytes)
-links = root.xpath("//a[contains(@href, 'caselaw')]")
-for link in links:
-    link.tail = ""
-    print(lxml.html.tostring(link))
+# links = root.xpath("//a[contains(@href, 'caselaw')]")
+# for link in links:
+#     link.tail = ""
+#     print(lxml.html.tostring(link))
 
-print(len(links))
+# print(len(links))
