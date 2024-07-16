816a26f1c7f9e483d95074eec63d7d6eb4fed1e4
Dragon Dave McKee <david.mckee@dxw.com>
2024-07-16 12:31:10 +0100

    demo selects judgments

--- demo.py
+++ demo.py
@@ -20,14 +20,19 @@ api_client = Client.MarklogicApiClient(
 # print(Client.__file__)
 # print(api_client.get_linked_caselaw("uksc/2024/1"))
 
+namespaces = {
+    "akn": "http://docs.oasis-open.org/legaldocml/ns/akn/3.0",
+    "uk": "https://caselaw.nationalarchives.gov.uk/akn",
+}
+
 raw_bytes = Client.get_single_bytestring_from_marklogic_response(
     api_client.accessible_judgment_transformation("uksc/2024/1", show_unpublished=True)
 )
 root = lxml.html.fromstring(raw_bytes)
 print(raw_bytes)
-# links = root.xpath("//a[contains(@href, 'caselaw')]")
-# for link in links:
-#     link.tail = ""
-#     print(lxml.html.tostring(link))
+links = root.xpath("//ref[contains(@href, 'caselaw')]", namespaces=namespaces)
+for link in links:
+    link.tail = ""
+    print(lxml.html.tostring(link))
 
-# print(len(links))
+print(len(links))
