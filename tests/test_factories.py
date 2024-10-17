e3e66662e0ddf0f43e9703c8bb670d608d651e9e
David McKee <david.mckee@dxw.com>
2024-10-17 18:15:08 +0100

    Ensure Judgment- and PressSummaryFactory have working NCNs

--- tests/test_factories.py
+++ tests/test_factories.py
@@ -1,6 +1,23 @@
-from caselawclient.factories import DocumentFactory
+import pytest
+
+from caselawclient.factories import DocumentFactory, JudgmentFactory, PressSummaryFactory
 
 
 def test_content_as_html():
     doc = DocumentFactory.build()
     assert doc.content_as_html() == "<p>This is a judgment.</p>"
+
+
+class TestNCNBehaviour:
+    def test_ncn_judgment(self):
+        doc = JudgmentFactory.build(neutral_citation="not the default")
+        assert doc.neutral_citation == "not the default"
+
+    def test_ncn_press(self):
+        doc = PressSummaryFactory.build(neutral_citation="not the default")
+        assert doc.neutral_citation == "not the default"
+
+    def test_ncn_doc(self):
+        doc = DocumentFactory.build(neutral_citation="not the default")
+        with pytest.raises(AttributeError):
+            doc.neutral_citation
