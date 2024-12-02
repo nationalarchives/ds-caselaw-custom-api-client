a6ed10319546f35e7785ce37ad441a7b4671008f
David McKee <david.mckee@dxw.com>
2024-12-02 14:32:02 +0000

    Migrate functions to identifiers class, add CRUD

--- tests/models/documents/test_document_identifiers.py
+++ tests/models/documents/test_document_identifiers.py
@@ -8,23 +8,23 @@ class TestDocumentIdentifiers:
     def test_add_identifiers(self):
         document = DocumentFactory.build()
 
-        identifier_1 = TestIdentifier(uuid="e28e3ef1-85ed-4997-87ee-e7428a6cc02e", value="TEST-123")
-        identifier_2 = TestIdentifier(uuid="14ce4b3b-03c8-44f9-a29e-e02ce35fe136", value="TEST-456")
-        document.add_identifier(identifier_1)
-        document.add_identifier(identifier_2)
+        identifier_1 = TestIdentifier(uuid="id-1", value="TEST-123")
+        identifier_2 = TestIdentifier(uuid="id-2", value="TEST-456")
+        document.identifiers.add(identifier_1)
+        document.identifiers.add(identifier_2)
 
-        assert document.identifiers == [
-            identifier_1,
-            identifier_2,
-        ]
+        assert document.identifiers == {
+            "id-1": identifier_1,
+            "id-2": identifier_2,
+        }
 
     def test_identifiers_as_etree(self):
         document = DocumentFactory.build()
 
         identifier_1 = TestIdentifier(uuid="e28e3ef1-85ed-4997-87ee-e7428a6cc02e", value="TEST-123")
         identifier_2 = TestIdentifier(uuid="14ce4b3b-03c8-44f9-a29e-e02ce35fe136", value="TEST-456")
-        document.add_identifier(identifier_1)
-        document.add_identifier(identifier_2)
+        document.identifiers.add(identifier_1)
+        document.identifiers.add(identifier_2)
 
         expected_xml = """
             <identifiers>
@@ -43,6 +43,6 @@ class TestDocumentIdentifiers:
             </identifiers>
         """
 
-        assert etree.canonicalize(document.identifiers_as_etree, strip_text=True) == etree.canonicalize(
+        assert etree.canonicalize(document.identifiers.as_etree, strip_text=True) == etree.canonicalize(
             etree.fromstring(expected_xml), strip_text=True
         )
