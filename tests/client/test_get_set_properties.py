ffc645a5a98971545cdf035445131b82daabbb76
Nick Jackson <nick@dxw.com>
2025-08-19 12:18:01 +0100

    test(MarklogicApiClient): better break up tests for getting/setting document properties
    This gives us more structured tests for different kinds of properties.

--- tests/client/test_get_set_properties.py
+++ tests/client/test_get_set_properties.py
@@ -1,14 +1,65 @@
 import json
 import os
-import unittest
 from unittest.mock import ANY, patch
 
 from caselawclient.Client import ROOT_DIR, MarklogicApiClient
 from caselawclient.models.documents import DocumentURIString
 
 
-class TestGetSetJudgmentProperties(unittest.TestCase):
-    def setUp(self):
+class TestGetSetStringDocumentProperties:
+    """Test cases for getting and setting document properties which are strings."""
+
+    def setup_method(self):
+        self.client = MarklogicApiClient("", "", "", False)
+
+    def test_get_property(self):
+        with patch.object(self.client, "eval") as mock_eval:
+            mock_eval.return_value.text = "my-content"
+            mock_eval.return_value.headers = {
+                "content-type": "multipart/mixed; boundary=595658fa1db1aa98",
+            }
+            mock_eval.return_value.content = (
+                b"\r\n--595658fa1db1aa98\r\n"
+                b"Content-Type: text/plain\r\n"
+                b"X-Primitive: text()\r\n"
+                b"X-URI: /ewca/civ/2004/632.xml\r\n"
+                b"X-Path: /prop:properties/published/text()\r\n"
+                b"\r\nmy-content\r\n"
+                b"--595658fa1db1aa98--\r\n"
+            )
+            result = self.client.get_property(DocumentURIString("judgment/uri"), "my-property")
+
+            assert result == "my-content"
+
+    def test_get_unset_property(self):
+        with patch.object(self.client, "eval") as mock_eval:
+            mock_eval.return_value.content = ""
+            result = self.client.get_property(DocumentURIString("judgment/uri"), "my-property")
+
+            assert result == ""
+
+    def test_set_property(self):
+        with patch.object(self.client, "eval") as mock_eval:
+            uri = DocumentURIString("judgment/uri")
+            expected_vars = {
+                "uri": "/judgment/uri.xml",
+                "value": "my-value",
+                "name": "my-property",
+            }
+            self.client.set_property(uri, name="my-property", value="my-value")
+
+            mock_eval.assert_called_with(
+                os.path.join(ROOT_DIR, "xquery", "set_property.xqy"),
+                vars=json.dumps(expected_vars),
+                accept_header="application/xml",
+                timeout=ANY,
+            )
+
+
+class TestGetSetBooleanDocumentProperties:
+    """Test cases for getting and setting document properties which are booleans."""
+
+    def setup_method(self):
         self.client = MarklogicApiClient("", "", "", False)
 
     def test_set_boolean_property_true(self):
@@ -70,46 +121,3 @@ class TestGetSetJudgmentProperties(unittest.TestCase):
             result = self.client.get_boolean_property(DocumentURIString("judgment/uri"), "my-property")
 
             assert result is True
-
-    def test_get_property(self):
-        with patch.object(self.client, "eval") as mock_eval:
-            mock_eval.return_value.text = "my-content"
-            mock_eval.return_value.headers = {
-                "content-type": "multipart/mixed; boundary=595658fa1db1aa98",
-            }
-            mock_eval.return_value.content = (
-                b"\r\n--595658fa1db1aa98\r\n"
-                b"Content-Type: text/plain\r\n"
-                b"X-Primitive: text()\r\n"
-                b"X-URI: /ewca/civ/2004/632.xml\r\n"
-                b"X-Path: /prop:properties/published/text()\r\n"
-                b"\r\nmy-content\r\n"
-                b"--595658fa1db1aa98--\r\n"
-            )
-            result = self.client.get_property(DocumentURIString("judgment/uri"), "my-property")
-
-            assert result == "my-content"
-
-    def test_get_unset_property(self):
-        with patch.object(self.client, "eval") as mock_eval:
-            mock_eval.return_value.content = ""
-            result = self.client.get_property(DocumentURIString("judgment/uri"), "my-property")
-
-            assert result == ""
-
-    def test_set_property(self):
-        with patch.object(self.client, "eval") as mock_eval:
-            uri = DocumentURIString("judgment/uri")
-            expected_vars = {
-                "uri": "/judgment/uri.xml",
-                "value": "my-value",
-                "name": "my-property",
-            }
-            self.client.set_property(uri, name="my-property", value="my-value")
-
-            mock_eval.assert_called_with(
-                os.path.join(ROOT_DIR, "xquery", "set_property.xqy"),
-                vars=json.dumps(expected_vars),
-                accept_header="application/xml",
-                timeout=ANY,
-            )
